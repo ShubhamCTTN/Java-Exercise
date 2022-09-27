6d74e4be4e676e4b84f65d2a664508c0fef6840b
ShubhamCTTN <114070622+ShubhamCTTN@users.noreply.github.com>
2022-09-27 09:49:14 +0530

    Add files via upload

---
+++ Solution2.java
@@ -0,0 +1,20 @@
+/*Q2) Write a program to read user input until user writes XDONE and then show the entered text 
+by the user on commandline */
+
+package exercise;
+
+import java.util.*;
+public class Solution2 {	
+		
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String st;
+		String re = " ";
+		do {
+			st=sc.next();
+			re=re+" "+st;
+		}while(!st.equals("XDONE"));
+		System.out.println(re);
+	}
+}
+
---
+++ Solution3.java
@@ -0,0 +1,47 @@
+/*Q3) Write a java program to show following menu to the user:
+
+*******Menu*******
+1. Calculate Area of Circle
+2. Calculate Circumference of a Circle
+3. Exit.
+Choose an option (1-3): 
+
+Take radius as user input.
+
+Hint: Use Switch statement to act on the menu. Also area and circumference methods should be static*/
+
+package exercise;
+
+import java.util.Scanner;
+public class Solution3 {
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        double pai=3.14;
+        double r;
+        System.out.println("****** Manu *******") ;
+        System.out.println("1. Calculate Area of Circle");
+        System.out.println("2. Calculate Circumference of a Circle");
+        System.out.println("3. Exit.");
+        int i=sc.nextInt();
+        switch(i){
+
+            case 1: {
+                System.out.print("Enter the Radius :");
+                r= sc.nextDouble();
+                System.out.println("Area of Circle" +(pai));
+            }
+            case 2:{
+                System.out.print("Enter the Radius :");
+                r= sc.nextDouble();
+                System.out.println("Circumference of Circle" +(2*pai));
+
+            }
+            case 3:{
+                System.out.println("Exit");
+            }
+
+        }
+   }
+}
+
